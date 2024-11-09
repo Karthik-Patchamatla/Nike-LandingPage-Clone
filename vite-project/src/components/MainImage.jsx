12ebce7284b14a097c4e8315e7fe3866395d0b6f
Karthik-Patchamatla <pkarthikvarma830@gmail.com>
2024-11-09 12:44:09 +0530

    small changes in main image

--- vite-project/src/components/MainImage.jsx
+++ vite-project/src/components/MainImage.jsx
@@ -1,9 +1,19 @@
-import Mainimage from '../assets/mainimage.jpg';
+import MainImageSmall from '../assets/smallimage.jpg';
+import MainImageLarge from '../assets/largeimage.jpg';
 
 export default function MainImage() {
     return (
         <div className="mt-8">
-            <img src={Mainimage} className="h-[650px] w-full" alt="Main" />
+            <img
+                src={MainImageLarge}
+                className="h-[650px] w-full sm:hidden"
+                alt="Main for small screens"
+            />
+            <img
+                src={MainImageSmall}
+                className="hidden sm:block h-[650px] w-full"
+                alt="Main for larger screens"
+            />
         </div>
     );
 }
